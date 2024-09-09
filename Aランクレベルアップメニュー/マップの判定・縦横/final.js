89d7730476cac6800fcae4e701e089533fc1dd04
rifie536 <maitakaz536@gmail.com>
2024-09-09 20:22:11 +0900

    別解を考察

--- Aランクレベルアップメニュー/マップの判定・縦横/final.js
+++ Aランクレベルアップメニュー/マップの判定・縦横/final.js
@@ -58,3 +58,29 @@ main(require('fs').readFileSync('/dev/stdin', 'utf8'));
 // };
 
 // main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+// isWall関数はなくてもいい
+// const main = (input) => {
+//     const lines = input.trim().split('\n');
+//     const [H, W] = lines[0].split(' ').map(Number);
+//     const map = lines.slice(1);
+
+//     const checkSurroundings = (y, x) => {
+//         const up = y === 0 || map[y - 1][x] === '#';
+//         const down = y === H - 1 || map[y + 1][x] === '#';
+//         const left = x === 0 || map[y][x - 1] === '#';
+//         const right = x === W - 1 || map[y][x + 1] === '#';
+
+//         return up && down && left && right;
+//     };
+
+//     for (let y = 0; y < H; y++) {
+//         for (let x = 0; x < W; x++) {
+//             if (checkSurroundings(y, x)) {
+//                 console.log(`${y} ${x}`);
+//             }
+//         }
+//     }
+// };
+
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
