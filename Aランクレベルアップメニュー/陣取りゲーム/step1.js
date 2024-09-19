7026f93e47511a6622fe5236d930cf6f396e3472
rifie536 <maitakaz536@gmail.com>
2024-09-19 23:31:39 +0900

    Step1 JavaScript clear

--- Aランクレベルアップメニュー/陣取りゲーム/step1.js
+++ Aランクレベルアップメニュー/陣取りゲーム/step1.js
@@ -2,17 +2,19 @@ const main = (input) => {
     const lines = input.trim().split('\n');
     const [h, w] = lines[0].split(' ').map(Number);
     const board = lines.slice(1).map(line => line.split(''));
-    for (let i = 0; i < h; i++) {
-        for (let j = 0; j < w; j++) {
-            if (board[i][j] === '*') {
-                i > 0 ? board[i - 1][j] = '*' : '';
-                i < h - 1 ? board[i + 1][j] = '*' : '';
-                j > 0 ? board[i][j - 1] = '*' : '';
-                j < w - 1 ? board[i][j + 1] = '*' : '';
-                console.log(board.map(row => row.join('')).join('\n'));
-                return;
-            }
+    const playersTeritory = (h, w, board) => {
+        for (let i = 0; i < h; i++) {
+            for (let j = 0; j < w; j++) {
+                if (board[i][j] === '*') {
+                    i > 0 ? board[i - 1][j] = '*' : '';
+                    i < h - 1 ? board[i + 1][j] = '*' : '';
+                    j > 0 ? board[i][j - 1] = '*' : '';
+                    j < w - 1 ? board[i][j + 1] = '*' : '';
+                    return board.map(row => row.join('')).join('\n');
+                }
+             }
         }
     }
+    console.log(playersTeritory(h, w, board));
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
