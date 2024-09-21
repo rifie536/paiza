338799e979d09fbd2ac6b59890682daed6b3e363
rifie536 <maitakaz536@gmail.com>
2024-09-21 22:07:12 +0900

    Step3 JavaScript 75点

---
+++ Aランクレベルアップメニュー/陣取りゲーム/step3.js
@@ -0,0 +1,20 @@
+const main = (input) => {
+    const lines = input.trim().split('\n');
+    const [h, w] = lines[0].split(' ').map(Number);
+    const board = lines.slice(1).map(line => line.split(''));
+    const playersTerritory = (h, w, board) => {
+        for (let i = 0; i < h; i++) {
+            for (let j = 0; j < w; j++) {
+                if (board[i][j] === '*') {
+                    i > 0 && board[i - 1][j] != '#' ? board[i - 1][j] = '*' : '';
+                    i < h - 1 && board[i + 1][j] != '#' ? board[i + 1][j] = '*' : '';
+                    j > 0 && board[i][j - 1] != '#' ? board[i][j - 1] = '*' : '';
+                    j < w - 1 && board[i][j + 1] != '#' ? board[i][j + 1] = '*' : '';
+                }
+             }
+        }
+        return board.map(row => row.join('')).join('\n');
+    }
+    console.log(playersTerritory(h, w, board));
+}
+main(require('fs').readFileSync('/dev/stdin', 'Utf8'));
