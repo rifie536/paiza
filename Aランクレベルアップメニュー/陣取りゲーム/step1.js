91e5854087f4098528d729616ce955297ccaae14
rifie536 <maitakaz536@gmail.com>
2025-02-05 23:25:13 +0900

    add TS

--- Aランクレベルアップメニュー/陣取りゲーム/step1.js
+++ Aランクレベルアップメニュー/陣取りゲーム/step1.js
@@ -18,3 +18,29 @@ const main = (input) => {
     console.log(playersTerritory(h, w, board));
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+// TypeScript
+// const main = (input: string): void => {
+//     const lines: string[] = input.trim().split('\n');
+//     const [h, w]: number[] = lines[0].split(' ').map(Number);
+//     const board: string[][] = lines.slice(1).map(line => line.split(''));
+
+//     const playersTerritory = (h: number, w: number, board: string[][]): string => {
+//         for (let i = 0; i < h; i++) {
+//             for (let j = 0; j < w; j++) {
+//                 if (board[i][j] === '*') {
+//                     if (i > 0) board[i - 1][j] = '*';
+//                     if (i < h - 1) board[i + 1][j] = '*';
+//                     if (j > 0) board[i][j - 1] = '*';
+//                     if (j < w - 1) board[i][j + 1] = '*';
+//                     return board.map(row => row.join('')).join('\n');
+//                 }
+//             }
+//         }
+//         return '';
+//     }
+
+//     console.log(playersTerritory(h, w, board));
+// }
+
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
