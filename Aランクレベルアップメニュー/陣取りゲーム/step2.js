ba9d82cbf93ec758eee5c741a28a2c9906a7cc24
rifie536 <maitakaz536@gmail.com>
2025-02-06 22:42:14 +0900

    Add TypeScript

--- Aランクレベルアップメニュー/陣取りゲーム/step2.js
+++ Aランクレベルアップメニュー/陣取りゲーム/step2.js
@@ -18,3 +18,30 @@ const main = (input) => {
     console.log(playersTerritory(h, w, board));
 }
 main(require('fs').readFileSync('/dev/stdin', 'Utf8'));
+
+// TypeScript
+
+// const main = (input: string): void => {
+//     const lines: string[] = input.trim().split('\n');
+//     const [h, w]: number[] = lines[0].split(' ').map(Number);
+//     const board: string[][] = lines.slice(1).map(line => line.split(''));
+
+//     const playersTerritory = (h: number, w: number, board: string[][]): string => {
+//         for (let i = 0; i < h; i++) {
+//             for (let j = 0; j < w; j++) {
+//                 if (board[i][j] === '*') {
+//                     i > 0 && board[i - 1][j] !== '#' ? board[i - 1][j] = '*' : '';
+//                     i < h - 1 && board[i + 1][j] !== '#' ? board[i + 1][j] = '*' : '';
+//                     j > 0 && board[i][j - 1] !== '#' ? board[i][j - 1] = '*' : '';
+//                     j < w - 1 && board[i][j + 1] !== '#' ? board[i][j + 1] = '*' : '';
+//                     return board.map(row => row.join('')).join('\n');
+//                 }
+//             }
+//         }
+//         return '';
+//     }
+
+//     console.log(playersTerritory(h, w, board));
+//  }
+
+//  main(require('fs').readFileSync('/dev/stdin', 'utf8'));
