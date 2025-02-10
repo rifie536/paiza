d3397a945972109ea096620e99373e838e85d5fa
rifie536 <maitakaz536@gmail.com>
2025-02-10 23:06:26 +0900

    Add TS

--- Aランクレベルアップメニュー/座標系での向きの変わる移動/step2.js
+++ Aランクレベルアップメニュー/座標系での向きの変わる移動/step2.js
@@ -17,3 +17,28 @@ const main = (input) => {
     }
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+
+// TypeScript
+
+// const main = (input: string): void => {
+//     const lines: string[] = input.trim().split('\n');
+//     const [y, x, n]: number[] = lines[0].split(' ').map(Number);
+//     const directions: string[] = lines.slice(1);
+//     let currentY: number = y;
+//     let currentX: number = x;
+ 
+//     const operations: Record<string, () => void> = {
+//         'N': () => currentY--,
+//         'E': () => currentX++,
+//         'W': () => currentX--,
+//         'S': () => currentY++,
+//     };
+ 
+//     for (const direction of directions) {
+//         operations[direction]();
+//         console.log(`${currentY} ${currentX}`);
+//     }
+//  }
+ 
+//  main(require('fs').readFileSync('/dev/stdin', 'utf8'));
