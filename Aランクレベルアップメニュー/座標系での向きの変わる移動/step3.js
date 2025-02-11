6f851ab37c4291850090009aef9eb88187488da0
rifie536 <maitakaz536@gmail.com>
2025-02-11 21:28:45 +0900

    Add TS

--- Aランクレベルアップメニュー/座標系での向きの変わる移動/step3.js
+++ Aランクレベルアップメニュー/座標系での向きの変わる移動/step3.js
@@ -14,3 +14,26 @@ const main = (input) => {
     console.log(`${currentY} ${currentX}`);
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+
+// TypeScript
+
+// const main = (input: string): void => {
+//     const lines: string[] = input.trim().split('\n');
+//     const [y, x, d]: string[] = lines[0].split(' ');
+//     const direction: string = lines[1];
+//     let currentY: number = Number(y);
+//     let currentX: number = Number(x);
+ 
+//     const operation: Record<string, (direction: string) => void> = {
+//         'N': (direction: string) => {direction === 'L' ? currentX-- : currentX++},
+//         'E': (direction: string) => {direction === 'L' ? currentY-- : currentY++},
+//         'W': (direction: string) => {direction === 'L' ? currentY++ : currentY--},
+//         'S': (direction: string) => {direction === 'L' ? currentX++ : currentX--},
+//     };
+ 
+//     operation[d](direction);
+//     console.log(`${currentY} ${currentX}`);
+//  }
+ 
+//  main(require('fs').readFileSync('/dev/stdin', 'utf8'));
