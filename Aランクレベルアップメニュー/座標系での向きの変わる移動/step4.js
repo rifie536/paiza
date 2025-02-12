cf25a087c048035227ba8064a370cce2c2244f4a
rifie536 <maitakaz536@gmail.com>
2025-02-12 21:48:14 +0900

    Add TS

--- Aランクレベルアップメニュー/座標系での向きの変わる移動/step4.js
+++ Aランクレベルアップメニュー/座標系での向きの変わる移動/step4.js
@@ -26,3 +26,38 @@ const main = (input) => {
     console.log(`${currentX} ${currentY}`);
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+// TypeScript
+
+// const main = (input: string): void => {
+//     const [x, y, n]: number[] = input.trim().split(' ').map(Number);
+//     let currentX: number = x;
+//     let currentY: number = y;
+//     let currentDirection: number = 0;
+ 
+//     const operations: Record<number, () => void> = {
+//         0: () => currentX++, // East
+//         1: () => currentY++, // South
+//         2: () => currentX--, // West
+//         3: () => currentY--, // North
+//     };
+ 
+//     let moveAmount: number = 1;
+//     let changedDirectionCount: number = 0;
+//     let count: number = 0;
+ 
+//     while (count < n) {
+//         for (let i = 0; i < moveAmount && count < n; i++) {
+//             operations[currentDirection]();
+//             count++;
+//         }
+//         currentDirection = (currentDirection + 1) % 4;
+//         changedDirectionCount++;
+//         if (changedDirectionCount % 2 === 0) {
+//             moveAmount++;
+//         }
+//     }
+//     console.log(`${currentX} ${currentY}`);
+//  }
+ 
+//  main(require('fs').readFileSync('/dev/stdin', 'utf8'));
