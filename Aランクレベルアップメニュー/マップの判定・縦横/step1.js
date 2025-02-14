e5e6a7cc5c556816bbfa419a3e12f50cae8085cd
rifie536 <maitakaz536@gmail.com>
2025-02-14 22:01:32 +0900

    Add TS

--- Aランクレベルアップメニュー/マップの判定・縦横/step1.js
+++ Aランクレベルアップメニュー/マップの判定・縦横/step1.js
@@ -22,3 +22,20 @@ main(require('fs').readFileSync('/dev/stdin', 'utf8'));
 //     })
 // }
 // main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+// TypeScript
+
+// const main = (input: string): void => {
+//     const lines: string[] = input.trim().split('\n');
+//     const [h, w, n]: number[] = lines[0].split(' ').map(Number);
+//     const s: string[] = lines.slice(1, h + 1);
+//     const yx: number[][] = lines.slice(h + 1).map(line => line.split(' ').map(Number));
+ 
+//     for (let i = 0; i < n; i++) {
+//         const y: number = yx[i][0];
+//         const x: number = yx[i][1];
+//         console.log(s[y][x]);
+//     }
+//  }
+ 
+//  main(require('fs').readFileSync('/dev/stdin', 'utf8'));
