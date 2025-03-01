1fc607681fc9f0488a16e9bd8b2af016b9ae1ef6
rifie536 <maitakaz536@gmail.com>
2025-03-01 22:31:55 +0900

    Add TS

--- 新・Bランクレベルアップメニュー/マップの扱い/one.js
+++ 新・Bランクレベルアップメニュー/マップの扱い/one.js
@@ -7,3 +7,19 @@ const main = (input) => {
     console.log(strings.map(string => string.join('')).join('\n'));
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+// TypeScript
+
+// const main = (input: string): void => {
+//     const lines: string[] = input.trim().split('\n');
+//     const [h, w] = lines.shift()!.split(' ').map(Number);
+//     const [y, x] = lines.pop()!.split(' ').map(Number);
+    
+//     let strings: string[][] = lines.map(string => string.split(''));
+//     strings[y][x] = strings[y][x] === '.' ? '#' : '.';
+    
+//     console.log(strings.map(string => string.join('')).join('\n'));
+// };
+
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
