dc91fa3e9a3797f2412a67acdb8ccd3c0d544ad2
rifie536 <maitakaz536@gmail.com>
2025-03-02 20:06:53 +0900

    Add TS

--- 新・Bランクレベルアップメニュー/マップの扱い/two.js
+++ 新・Bランクレベルアップメニュー/マップの扱い/two.js
@@ -16,3 +16,29 @@ const main = (input) => {
     console.log(strings.map(string => string.join('')).join('\n'));
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+
+// TypeScript
+
+// const main = (input: string): void => {
+//     const lines: string[] = input.trim().split('\n');
+//     const [h, w]: number[] = lines.shift()!.split(' ').map(Number);
+//     const [y, x]: number[] = lines.pop()!.split(' ').map(Number);
+//     let strings: string[][] = lines.map(string => string.split(''));
+    
+//     const inversion = (y: number, x: number): void => {
+//         if (y >= 0 && y < h && x >= 0 && x < w) {
+//             strings[y][x] = strings[y][x] === '.' ? '#' : '.';
+//         }
+//     };
+    
+//     inversion(y, x);
+//     inversion(y - 1, x);
+//     inversion(y + 1, x);
+//     inversion(y, x - 1);
+//     inversion(y, x + 1);
+    
+//     console.log(strings.map(string => string.join('')).join('\n'));
+// };
+
+//  main(require('fs').readFileSync('/dev/stdin', 'utf8'));
