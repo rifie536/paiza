4d1cf18325d3b0f17a9bf80a0fda5365d4e6dcb8
rifie536 <maitakaz536@gmail.com>
2025-03-05 22:32:58 +0900

    Add TS

--- 新・Bランクレベルアップメニュー/計算1/q2.js
+++ 新・Bランクレベルアップメニュー/計算1/q2.js
@@ -14,3 +14,25 @@ const main = (input) => {
     generateRndmNum(x, m ,n);
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+
+// TypeScript
+
+// const main = (input: string): void => {
+//     const [x, m, n] = input.trim().split(' ').map(Number);
+    
+//     const generateRndmNum = (x: number, m: number, n: number): void => {
+//         let powNum = 1;
+//         let rndNum = 0;
+        
+//         for (let i = 0; i < n; i++) {
+//             powNum = (powNum * x) % m;
+//             rndNum = (rndNum + powNum) % m;
+//             console.log(rndNum);
+//         }
+//     }
+    
+//     generateRndmNum(x, m, n);
+// }
+
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
