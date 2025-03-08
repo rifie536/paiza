1284a7742571d3c0d7d7cc24d0b1e22881ed8163
rifie536 <maitakaz536@gmail.com>
2025-03-08 23:40:01 +0900

    Add TS

--- 新・Bランクレベルアップメニュー/文字列/q1.js
+++ 新・Bランクレベルアップメニュー/文字列/q1.js
@@ -16,3 +16,25 @@ const main = (input) => {
     }
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+
+// TypeScript
+
+// const main = (input) => {
+//     const str = input.trim();
+//     for (let i = 0; i < str.length; i++) {
+//         const copyStr = str.slice(i);
+//         let temp = '';
+//         for (let j = 0; j < copyStr.length; j++) {
+//             if (Number.isInteger(Number(copyStr[0]))) {
+//                 if (Number.isInteger(Number(copyStr[j]))) {
+//                     temp = temp + copyStr[j];
+//                     console.log(temp);
+//                 } else {
+//                     temp = temp + copyStr[j];
+//                 }
+//             }
+//         }
+//     }
+// }
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
