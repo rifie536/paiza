ce713b54e69032c471e78e929aa01c84ac34e07b
rifie536 <maitakaz536@gmail.com>
2025-03-09 22:37:17 +0900

    Add TS

--- 新・Bランクレベルアップメニュー/文字列/q2.js
+++ 新・Bランクレベルアップメニュー/文字列/q2.js
@@ -14,3 +14,23 @@ const main = (input) => {
 }
 
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+
+// TypeScript
+
+// const main = (input: string): void => {
+//     const X = input.trim();
+//     const hash = (X: string): number => {
+//         let sum = 0;
+//         for (let i = 0; i < 4; i++) {
+//             let part = X.substring(i * 8, (i + 1) * 8);
+//             sum += parseInt(part);
+//         }
+//         return sum;
+//     }
+
+//     const result = hash(X);
+//     console.log(result);
+// }
+
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
