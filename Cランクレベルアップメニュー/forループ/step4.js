63669b6bec121a3cd277a191a2aa93e27b85acde
rifie536 <maitakaz536@gmail.com>
2025-03-17 21:05:51 +0900

    Add TS

--- Cランクレベルアップメニュー/forループ/step4.js
+++ Cランクレベルアップメニュー/forループ/step4.js
@@ -12,3 +12,21 @@ const main = (input) => {
 }
 
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+
+// TypeScript
+
+// const main = (input: string): void => {
+//     const lines = input.trim().split('\n');
+//     const m = Number(lines[0]);
+//     const chars = lines.slice(1, m + 1);
+//     const n = Number(lines[m + 1]);
+//     const strings = lines.slice(m + 2);
+//     for (const char of chars) {
+//         for (const str of strings) {
+//             console.log(str.includes(char) ? 'YES' : 'NO');
+//         }
+//     }
+// }
+
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
