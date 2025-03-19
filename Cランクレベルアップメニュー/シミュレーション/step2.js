5c4ed0093fbdb19aa9114fd553b83dd8a22349e7
rifie536 <maitakaz536@gmail.com>
2025-03-19 22:19:15 +0900

    Add TS

--- Cランクレベルアップメニュー/シミュレーション/step2.js
+++ Cランクレベルアップメニュー/シミュレーション/step2.js
@@ -17,3 +17,26 @@ const main = (input) => {
     }
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+
+// TypeScript
+
+// const main = (input: string): void => {
+//     const lines = input.trim().split('\n');
+//     const n = Number(lines[0]);
+//     const [a, b] = lines[1].split(' ').map(Number);
+//     const nums: Record<string, number> = {
+//         'paiza': 1,
+//         '霧島京子': 1
+//     };
+
+//     for (let i = 1; ; i++) {
+//         nums['霧島京子'] += nums['paiza'] * a;
+//         if (nums['霧島京子'] > n) {
+//             console.log(i);
+//             return;
+//         }
+//         nums['paiza'] += nums['霧島京子'] % b;
+//     }
+// }
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
