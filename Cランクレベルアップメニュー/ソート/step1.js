b1f2deadc0fd64655256576622581caf025c3fb6
rifie536 <maitakaz536@gmail.com>
2025-03-21 22:12:30 +0900

    Add TS

--- Cランクレベルアップメニュー/ソート/step1.js
+++ Cランクレベルアップメニュー/ソート/step1.js
@@ -5,3 +5,13 @@ const main = (input) => {
 }
 
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+// TypeScript
+
+// const main = (input: string): void => {
+//     const [n, ...nums] = input.trim().split('\n').map(num => +num);
+//     const sortedNums = [...nums].sort((a, b) => a - b);
+//     console.log(sortedNums.join('\n'));
+// }
+
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
