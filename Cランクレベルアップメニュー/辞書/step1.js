b233406b13072df86fff1f5392c0f0a453c3a035
rifie536 <maitakaz536@gmail.com>
2025-03-24 23:34:28 +0900

    Add TS

--- Cランクレベルアップメニュー/辞書/step1.js
+++ Cランクレベルアップメニュー/辞書/step1.js
@@ -6,3 +6,16 @@ const main = (input) => {
     console.log(objSa[name]);
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+
+// TypeScript
+
+// const main = (input: string): void => {
+//     const [n, ...lines] = input.trim().split('\n');
+//     const name: string = lines.pop() || "";
+//     const sa: string[][] = lines.map(line => line.split(' '));
+//     const objSa: Record<string, string> = Object.fromEntries(sa);
+//     console.log(objSa[name]);
+// }
+
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
