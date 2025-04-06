6fa5a259b18f3d071d499c00fba054a1a1df25d7
rifie536 <maitakaz536@gmail.com>
2025-04-07 07:33:11 +0900

    Step3 Ruby Clear

---
+++ Cランクレベルアップメニュー/辞書/step3.rb
@@ -0,0 +1,13 @@
+n = gets.to_i
+s = {}
+n.times do
+    name = gets.chomp
+    s[name] = 0
+end
+m = gets.to_i
+m.times do
+    name, damage = gets.split(' ')
+    s[name] += damage.to_i
+end
+
+s.sort.each { |key, val| puts val }
