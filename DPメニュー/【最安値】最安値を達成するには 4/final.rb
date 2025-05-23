6900f99f36f58bd48fb085283ec4ca218a049111
rifie536 <maitakaz536@gmail.com>
2025-05-23 21:17:25 +0900

    Final Ruby Clear

---
+++ DPメニュー/【最安値】最安値を達成するには 4/final.rb
@@ -0,0 +1,23 @@
+n, x, a, y, b, z, c = gets.split.map(&:to_i)
+
+dp = Array.new(n + z, Float::INFINITY)
+dp[0] = 0
+
+for i in 1..n + z - 1
+    if i >= x
+        dp[i] = [dp[i - x] + a, dp[i]].min
+    end
+    if i >= y
+        dp[i] = [dp[i - y] + b, dp[i]].min
+    end
+    if i >= z
+        dp[i] = [dp[i - z] + c, dp[i]].min
+    end
+end
+
+result = Float::INFINITY
+(n..n + z - 1).each do |i|
+    result = [result, dp[i]].min
+end
+
+puts result
