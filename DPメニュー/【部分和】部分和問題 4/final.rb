5e8e50aca8be23a75f88c3baf4502248172897e7
rifie536 <maitakaz536@gmail.com>
2025-05-29 21:24:56 +0900

    Final Ruby Clear

---
+++ DPメニュー/【部分和】部分和問題 4/final.rb
@@ -0,0 +1,19 @@
+n, x = gets.split.map(&:to_i)
+a = n.times.map { gets.to_i }
+
+dp = Array.new(x + 1, false)
+dp[0] = true
+
+for i in 0...n
+    for j in a[i]..x
+        if dp[j - a[i]]
+            dp[j] = true
+        end
+    end
+end
+
+if dp[x]
+    puts "yes"
+else
+    puts "no"
+end
