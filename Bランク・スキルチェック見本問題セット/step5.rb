f94fcae3212872166edab3c5e835056311a22d65
rifie536 <maitakaz536@gmail.com>
2025-06-03 06:29:45 +0900

    Step5 Ruby Clear

---
+++ Bランク・スキルチェック見本問題セット/step5.rb
@@ -0,0 +1,14 @@
+n = gets.to_i
+a, b = gets.split.map(&:to_i)
+
+seat_num = []
+a.times do |i|
+    seat_index =  (b + i) % n
+    if seat_index == 0
+        seat_num << b + i
+    else
+        seat_num << seat_index
+    end
+end
+
+puts seat_num.join(' ')
