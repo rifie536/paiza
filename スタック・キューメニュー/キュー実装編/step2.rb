737d9089242b1a1dde00cc18ef2276e2b72af6de
rifie536 <maitakaz536@gmail.com>
2025-07-12 10:42:38 +0900

    Step2 Ruby Clear

---
+++ スタック・キューメニュー/キュー実装編/step2.rb
@@ -0,0 +1,12 @@
+q = gets.to_i
+arr = []
+q.times do
+    query = gets.split
+    if query[0].to_i == 1
+       arr.push(query[1])
+    else
+       puts arr.shift
+    end
+
+    puts arr.join(' ')
+end
